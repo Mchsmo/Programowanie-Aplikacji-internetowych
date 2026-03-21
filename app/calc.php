93791a0b6c2f5c7c6626b50ad7eb7c31af1d5f87
Mchsmo <twoj-email-z-nowego-konta@example.com>
2026-03-21 18:39:01 +0100

    Szablon Smarty

--- app/calc.php
+++ app/calc.php
@@ -1,80 +1,112 @@
 <?php
 require_once dirname(__FILE__).'/../config.php';
+
+require_once _ROOT_PATH.'/lib/smarty/libs/Smarty.class.php';
 $messages = [];
 
 // 1. Pobranie parametrów
-$kwota = $_REQUEST['kwota'] ?? null;
-$oprocentowanie = $_REQUEST['oprocentowanie'] ?? null;
-$lata = $_REQUEST['lata'] ?? null;
-
-// 2. Walidacja parametrów
-if (isset($_REQUEST['kwota']) && isset($_REQUEST['oprocentowanie']) && isset($_REQUEST['lata'])) {
-	if ($kwota == "" && $oprocentowanie == "" && $lata == "") {
-        $messages[] = 'Proszę wypełnić formularz.';
-    }
-
-    if ($kwota == "") {
-        $messages[] = 'Nie podano kwoty kredytu';
-    }
-
-    if ($oprocentowanie == "") {
-        $messages[] = 'Nie podano oprocentowania';
-    }
-
-    if ($lata == "") {
-        $messages[] = 'Nie podano planowanego czasu spłaty kredytu';
-    }
-
-	if (empty($messages)) {
-
-		if (!is_numeric($kwota)) {
-			$messages[] = 'Kwota kredytu nie jest liczbą';
-		}
-		if (!is_numeric($lata)) {
-			$messages[] = 'Czas trwania kredytu nie jest liczbą';
-		}
-		if (!is_numeric($oprocentowanie)) {
-			$messages[] = 'Oprocentowanie musi być liczbą';
-		}
-			
-		if ($kwota < 0) {
-			$messages[] = 'Kwota kredytu nie może być ujemna';
-		}
-
-		if ($lata <= 0) {
-			$messages[] = 'Liczba lat musi być większa od zera';
-		}
-
-		if ($oprocentowanie < 0) {
-			$messages[] = 'Oprocentowanie nie może być ujemne';
-		}
-	}
-
-	if (empty($messages)) {
-
-		// konwersja
-		$kwota = floatval($kwota);
-		$lata = intval($lata);
-		$oprocentowanie = floatval($oprocentowanie);
-
-		$liczba_rat = $lata * 12;
-
-		if ($oprocentowanie != 0) {
-
-			$op_mies = $oprocentowanie / (12 * 100);
-
-			$rata = $kwota * (
-				($op_mies * pow(1 + $op_mies, $liczba_rat)) /
-				(pow(1 + $op_mies, $liczba_rat) - 1)
-			);
-
-		} else {
-			$rata = $kwota / $liczba_rat;
-		}
-
-		$result = $rata;
-	}
+function getParams(&$form) {
+	$form['kwota'] = $_REQUEST['kwota'] ?? null;
+	$form['oprocentowanie'] = $_REQUEST['oprocentowanie'] ?? null;
+	$form['lata'] = $_REQUEST['lata'] ?? null;
 }
 
-include _ROOT_PATH.'/app/calc_view.php';
+// 2. Walidacja parametrów
+function validate(&$form, &$msgs, &$hide_intro) {
+	if (!(isset($form['kwota']) && isset($form['oprocentowanie']) && isset($form['lata']))) return false;
+	
+	$hide_intro = false;
+	
+	if ($form['kwota'] == "") {
+        $msgs[] = 'Nie podano kwoty kredytu';
+    }
+	if ($form['oprocentowanie'] == "") {
+        $msgs[] = 'Nie podano oprocentowania';
+    }
+	if ($form['lata'] == "") {
+        $msgs[] = 'Nie podano planowanego czasu spłaty kredytu';
+    }
+	if (empty($msgs)) {
+
+		if (!is_numeric($form['kwota'])) {
+			$msgs[] = 'Kwota kredytu nie jest liczbą';
+		}
+		if (!is_numeric($form['lata'])) {
+			$msgs[] = 'Czas trwania kredytu nie jest liczbą';
+		}
+		if (!is_numeric($form['oprocentowanie'])) {
+			$msgs[] = 'Oprocentowanie musi być liczbą';
+		}
+			
+		if ($form['kwota'] < 0) {
+			$msgs[] = 'Kwota kredytu nie może być ujemna';
+		}
+
+		if ($form['lata'] <= 0) {
+			$msgs[] = 'Liczba lat musi być większa od zera';
+		}
+
+		if ($form['oprocentowanie'] < 0) {
+			$msgs[] = 'Oprocentowanie nie może być ujemne';
+		}	
+	}
+	if (count($msgs)>0) return false;
+	else return true;
+}
+		
+function process(&$form, &$msgs, &$result) {
+	// konwersja
+	$kwota = floatval($form['kwota']);
+    $lata = intval($form['lata']);
+    $oprocentowanie = floatval($form['oprocentowanie']);
+	
+	$liczba_rat = $lata * 12;
+
+	if ($oprocentowanie != 0) {
+
+		$op_mies = $oprocentowanie / (12 * 100);
+
+		$rata = $kwota * (
+			($op_mies * pow(1 + $op_mies, $liczba_rat)) /
+			(pow(1 + $op_mies, $liczba_rat) - 1)
+		);
+
+	} else {
+		$rata = $kwota / $liczba_rat;
+	}
+
+	$result = $rata;
+}
+
+//inicjacja zmiennych
+$messages = array(); 
+$form = array();
+$result = null;
+$hide_intro = false;
+ 
+getParams($form);
+if ( validate($form,$messages,$hide_intro) ){
+	process($form,$messages,$result);
+}
+
+//Przygotowanie danych dla szablonu
+
+$smarty = new Smarty\Smarty();
+
+$smarty->setTemplateDir([
+    _ROOT_PATH.'/app/',        
+    _ROOT_PATH.'/templates/'   
+]);
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('title','Kalkulator kredytowy');
+
+$smarty->assign('hide_intro',$hide_intro);
+
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+
+$smarty->display(_ROOT_PATH.'/app/calc_view.html');
 ?>
---
+++ templates_c/d27eaa539c659a45dc965b55046aa946f3b1be07_0.file_main.html.php
@@ -0,0 +1,118 @@
+<?php
+/* Smarty version 5.5.1, created on 2026-03-20 07:38:27
+  from 'file:main.html' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.5.1',
+  'unifunc' => 'content_69bceb63a29902_11453105',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd27eaa539c659a45dc965b55046aa946f3b1be07' => 
+    array (
+      0 => 'main.html',
+      1 => 1773988348,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+))) {
+function content_69bceb63a29902_11453105 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\kalkulator_oprocentowania\\templates';
+$_smarty_tpl->getInheritance()->init($_smarty_tpl, false);
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title><?php echo (($tmp = $_smarty_tpl->getValue('page_title') ?? null)===null||$tmp==='' ? "Kalkulator Kredytowy" ?? null : $tmp);?>
+</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->getValue('app_url');?>
+/assets/css/main.css" />
+    <style>html { scroll-behavior: smooth; }</style>
+</head>
+<body class="is-preload">
+    <div id="wrapper">
+        
+                <?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_108129317069bceb63a26cf9_79350997', 'intro');
+?>
+
+
+        <div id="main">
+            <div class="inner">
+                <section id="app_content">
+                    <?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_62244384369bceb63a28970_55366281', 'content');
+?>
+
+                </section>
+            </div>
+        </div>
+
+        <footer id="footer">
+            <div class="inner">
+                <ul class="copyright">
+                    <li>Kalkulator Kredytowy</li>
+                    <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+
+    <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->getValue('app_url');?>
+/assets/js/jquery.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->getValue('app_url');?>
+/assets/js/main.js"><?php echo '</script'; ?>
+>
+</body>
+</html><?php }
+/* {block 'intro'} */
+class Block_108129317069bceb63a26cf9_79350997 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\kalkulator_oprocentowania\\templates';
+?>
+
+            <?php if (!(true && ($_smarty_tpl->hasVariable('result') && null !== ($_smarty_tpl->getValue('result') ?? null))) && $_smarty_tpl->getSmarty()->getModifierCallback('count')($_smarty_tpl->getValue('messages')) == 0) {?>
+                <header id="header" style="height: 100vh; display: flex; flex-direction: column; justify-content: center; background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('<?php echo $_smarty_tpl->getValue('app_url');?>
+/images/pic16.jpg'); background-size: cover; background-position: center; color: white; padding: 2em;">
+                    <div class="inner">
+                        <a href="<?php echo $_smarty_tpl->getValue('app_url');?>
+" class="logo">
+                            <span class="symbol"><img src="<?php echo $_smarty_tpl->getValue('app_url');?>
+/images/logo.svg" alt="" /></span>
+                            <span class="title">Kalkulator Kredytowy</span>
+                        </a>
+                        <div style="margin-top: 2em;">
+                            <ul class="actions">
+                                <li><a href="#app_content" class="button primary">Zacznij liczyć</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+            <?php }?>
+        <?php
+}
+}
+/* {/block 'intro'} */
+/* {block 'content'} */
+class Block_62244384369bceb63a28970_55366281 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\kalkulator_oprocentowania\\templates';
+?>
+ 
+                         
+                    <?php
+}
+}
+/* {/block 'content'} */
+}
